945f1ddfc252004a84c06a0a87888c5dd93f4cc9
thekalyugh <ashwani01superman@gmail.com>
2022-10-29 18:49:11 +0530

    ...._..

---
+++ back-end/models/compModel.js
@@ -0,0 +1,11 @@
+const { model, Schema, Types } = require('../connection');
+
+const mySchema = new Schema({
+    title: String,
+    code: String,
+    thumbnail: String,
+    uploadedBy: {type : Types.ObjectId, ref: 'users' },
+    createdAt: Date
+})
+
+module.exports = model('usersCollection', mySchema);
